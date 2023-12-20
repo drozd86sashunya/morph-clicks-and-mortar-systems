91d75fabb27ed5b38d956967b846f5de2ab40b5c
drozd86sashunya <152299288+drozd86sashunya@users.noreply.github.com>
2023-12-20 02:49:13 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    return data
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    main()
+def generate_random_data():
+
+    for item in data:
+    data = generate_random_data()
+
+def main():            System.out.println("Random Number: " + item);
+
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        for (int item : data) {
+        int[] data = new int[10];
+        }
+    data = generate_random_data()
+import random
+
+def main():
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+def generate_random_data():
+
+    main()
+    for item in data:
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
